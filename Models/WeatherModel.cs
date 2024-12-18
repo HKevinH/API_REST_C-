8b3a1b1ef07f024daa34fad186e5e4a6d59813c2
HKevinH <kevin.alvear00@usc.edu.co>
2024-12-17 23:28:56 -0500

    Make WeatherModel properties nullable

--- Models/WeatherModel.cs
+++ Models/WeatherModel.cs
@@ -2,10 +2,10 @@
 
 public class WeatherModel
 {
-    public string City { get; set; }
-    public string Country { get; set; }
+    public string? City { get; set; }
+    public string? Country { get; set; }
     public float Temperature { get; set; }
-    public string Condition { get; set; }
+    public string? Condition { get; set; }
 
     public DateTime Date { get; set; }
 }
